74df3cb01fbd0c2f7e5ff95baef5ae07890ef885
James Norris <James.Norris153@gmail.com>
2021-09-20 20:22:36 +0100

    Spliting up imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+
 import { RecipeCardComponent } from './recipe/recipe-card/recipe-card.component'
 import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component'
 
