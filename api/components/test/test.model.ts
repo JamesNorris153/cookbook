cfd2c9a4ab79f34e1ede6538e4c7c11cd6400bc7
James Norris <James.Norris153@gmail.com>
2021-11-10 20:45:12 +0000

    reordering optional parameter

--- api/components/test/test.model.ts
+++ api/components/test/test.model.ts
@@ -2,6 +2,6 @@ import { ObjectId } from 'mongodb';
 
 export class Test {
   public constructor(
-    public id?: ObjectId,
-    public name: string) { }
+    public name: string,
+    public id?: ObjectId) { }
 }
