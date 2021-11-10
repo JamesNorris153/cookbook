00af11409a005684019b9a039aca9fb0bbd3bf84
James Norris <James.Norris153@gmail.com>
2021-11-10 21:38:42 +0000

    Adding controller base class

--- api/components/test/test.controller.ts
+++ api/components/test/test.controller.ts
@@ -1,33 +1,17 @@
-import { Collection } from 'mongodb';
 import { Request, Response, Router } from 'express';
 
+import { Controller } from '../controller';
 import { Test } from './test.model';
 
-export class TestController {
-  private _router: Router | undefined;
-
-  constructor(
-    private readonly tests: Collection
-  ) { }
-
-  public get router(): Router {
-    if (!this._router) {
-      this.initialiseRoutes();
-    }
-
-    return this._router as Router;
+export class TestController extends Controller {
+  protected override initialiseRoutes(router: Router): void {
+    router.get('/', (request, response) => this.getTests(request, response));
+    router.post('/', (request, response) => this.postTest(request, response));
   }
 
-  private initialiseRoutes(): void {
-    this._router = Router();
-
-    this._router.get('/', (request, response) => this.getTests(request, response));
-    this._router.post('/', (request, response) => this.postTest(request, response));
-  }
-
-  private async getTests(_: Request, response: Response): Promise<void> {
+  private async getTests(_request: Request, response: Response): Promise<void> {
     try {
-      const tests = (await this.tests.find({}).toArray()) as Test[];
+      const tests = (await this.collection.find({}).toArray()) as Test[];
       response.status(200).json(tests);
     } catch (error) {
       response.status(500).json(error.message);
@@ -37,10 +21,10 @@ export class TestController {
   private async postTest(request: Request, response: Response): Promise<void> {
     try {
       const test = request.body as Test;
-      const result = await this.tests.insertOne(test);
+      const result = await this.collection.insertOne(test);
 
       result
-        ? response.status(200).json(result.acknowledged)
+        ? response.status(200).json(result.insertedId.toJSON())
         : response.status(500).send("bad thing");
     } catch (error) {
       response.status(500).send(error.message);
