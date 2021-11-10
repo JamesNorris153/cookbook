66cc69a5d304efb18b9dd86910bf3250df10290b
James Norris <James.Norris153@gmail.com>
2021-11-10 20:43:12 +0000

    Removing InMemoryDataService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { InMemoryDataService } from './in-memory-data.service';
 import { RecipeCardComponent } from './recipe/recipe-card/recipe-card.component';
 import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
 
@@ -20,9 +18,6 @@ import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component'
     AppRoutingModule,
     BrowserModule,
     FormsModule,
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    ),
     HttpClientModule
   ],
   providers: [],
